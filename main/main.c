61c2db32669df1c39d6944a9fb63fe860cd84b8b
Fabio Scarpa <fscarpa20@gmail.com>
2026-01-16 04:12:14 +0100

    better organize building blocks folder

--- main/main.c
+++ main/main.c
@@ -1,50 +1,44 @@
 #include <stdio.h>
-#include "esp_timer.h"      //used for timer
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "string.h"
 
-void timer_callback(void *args)             //timer callback implementation, always return void and take void *args as param
-                                            //will be called after timer trigger
-{
-    static bool on;
-    on = !on;
-    gpio_set_level(GPIO_NUM_4, on);
-    //printf("CALLED\n");
-}
+#define TXD_PIN 4
+#define RXD_PIN 5
+
+#define RX_BUF_SIZE 1024
 
 void app_main(void)
 {
+  uart_config_t uart_config = {                                     //configure serial port
+                                                                    //keys:
+                                                                    
+    .baud_rate = 9600,                                              //baud rate
+    .data_bits = UART_DATA_8_BITS,                                  //send 8 bits at a time
+    .parity = UART_PARITY_DISABLE,                                  //
+    .stop_bits = UART_STOP_BITS_1,                                  //
+    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE                           //
+  };
+  uart_param_config(UART_NUM_1 ,&uart_config);          //esp have 3 uart 0 is for serial communication so we use 1
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+  uart_set_pin(UART_NUM_1,TXD_PIN,RXD_PIN,UART_PIN_NO_CHANGE,UART_PIN_NO_CHANGE);   //set pins
+  uart_driver_install(UART_NUM_1,RX_BUF_SIZE,0,0,NULL,0);                           //install uart driver
+                                                                                        //1.uart we are using
+                                                                                        //2. buffer size receiver
+                                                                                        //3. buffer size sender 
+                                                                                        //4. queue size (not used in this example)
+                                                                                        //5. uart queue (NULL here)
+                                                                                        //6. interrupt flag 
 
-    const esp_timer_create_args_t esp_timer_create_args = {             //create a struct for timer
-                                                                            //keys:
-                                                                            //1. timer callback
-                                                                            //2. timer name
-        .callback = timer_callback,
-        .name = "My timer"
-    };
+  char message[] = "ping";
+  printf("sending: %s\n", message);
+  uart_write_bytes(UART_NUM_1,message, sizeof(message));                //send message through serial port 
 
-    esp_timer_handle_t esp_timer_handle;                                //create timer handler, it enables the timer to
-                                                                        //understand which tmer we want to use
+  //set up message tobe received
+  char incoming_message[RX_BUF_SIZE];                       
+  memset(incoming_message, 0 , sizeof(incoming_message));       //set all bytes pointed by incoming_message to 0 (clean the buffer)
+  uart_read_bytes(UART_NUM_1,(uint8_t *) incoming_message,RX_BUF_SIZE,pdMS_TO_TICKS(500));
+  printf("received: %s\n",incoming_message);
+  
 
-
-    esp_timer_create(&esp_timer_create_args, &esp_timer_handle);        //create timer with previous struct and handler
-
-    //2 function to start timers
-    //esp_timer_start_once(esp_timer_handle,20);                  //fire once after 2nd arg microseconds (min 20 us)
-    esp_timer_start_periodic(esp_timer_handle, ((1000*1000)*3));             //fire periodically (min 50 us)
-
-    int x = 0;
-    while (true)
-    {
-        esp_timer_dump(stdout);         //prints some info about timer
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        if (x++ == 5)
-        {
-            esp_timer_stop(esp_timer_handle);           //stop the timer at specified handler
-            esp_timer_delete(esp_timer_handle);         //clean data about timer at specified handler
-        }
-    }
 }
