12c61d3f7278062d2d393db2af604efd7afcb6f9
Fabio Scarpa <fscarpa20@gmail.com>
2026-01-20 05:12:15 +0100

    API call in progress

--- main/main.c
+++ main/main.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 #include "wifi_connect.h"
-#include "esp_http_client.h"    //allows the esp32 to make http requests
+#include "esp_http_client.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
 
 static const char *TAG = "REST";
 
-esp_err_t on_client_data(esp_http_client_event_t *evt)    //this handle events
+typedef struct chunk_payload_t          //struct to store incoming data
+{
+    uint8_t *buffer;
+    int buffer_index;
+} chunk_payload_t;
+
+esp_err_t on_client_data(esp_http_client_event_t *evt)
 {
     switch (evt->event_id)
     {
     case HTTP_EVENT_ON_DATA:
-        ESP_LOGI(TAG, "Length=%d", evt->data_len);
-        printf("%.*s\n", evt->data_len, (char *)evt->data);   //%.*s allows to print a string passing length 
-                                                              //and the string itself
-        break;
+    {
+        // ESP_LOGI(TAG, "Length=%d", evt->data_len);
+        // printf("%.*s\n", evt->data_len, (char *)evt->data);
+        chunk_payload_t *chunk_payload = evt->user_data;            //get user data
+
+        //realloc change the amount of space the payload is using
+        //1. existing size
+        //2. what size do we want
+        chunk_payload->buffer = realloc(chunk_payload->buffer, chunk_payload->buffer_index + evt->data_len + 1);
+
+        //copy data inside payload
+        //1. target index
+        //2. data
+        //3. how many bytes to copy
+        memcpy(&chunk_payload->buffer[chunk_payload->buffer_index],(uint8_t *) evt->data, evt->data_len );      
+        chunk_payload->buffer_index = chunk_payload->buffer_index + evt->data_len;      //move buffer index forward
+        chunk_payload->buffer[chunk_payload->buffer_index] = 0;
+
+        printf("buffer******** %s\n",chunk_payload->buffer);
+
+    }
+    break;
 
     default:
         break;
@@ -24,23 +49,28 @@ esp_err_t on_client_data(esp_http_client_event_t *evt)    //this handle events
 
 void fetch_quote()
 {
+    chunk_payload_t chunk_payload = {0};        //istance of payload, set the whole struct to 0
+
     esp_http_client_config_t esp_http_client_config = {
-        .url = "http://api.quotable.io/random",         
+        .url = "http://api.quotable.io/random",
         .method = HTTP_METHOD_GET,
-        .event_handler = on_client_data   //callback function               
-      };
+        .event_handler = on_client_data,
+        .user_data = &chunk_payload         //store incoming data
+    };
     esp_http_client_handle_t client = esp_http_client_init(&esp_http_client_config);
-    esp_http_client_set_header(client, "Content-Type", "application/json");
-    esp_err_t err = esp_http_client_perform(client);    //call the callback function
+    esp_http_client_set_header(client, "Contnet-Type", "application/json");
+    esp_err_t err = esp_http_client_perform(client);
     if (err == ESP_OK)
     {
-        ESP_LOGI(TAG, "HTTP GET status = %d, content_length = %d",
-                 esp_http_client_get_status_code(client),
-                 esp_http_client_get_content_length(client));
+        ESP_LOGI(TAG, "HTTP GET status = %d, result = %s\n",
+                 esp_http_client_get_status_code(client),  chunk_payload.buffer);
     }
     else
     {
         ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    if(chunk_payload.buffer != NULL){           //free memory we created
+       free(chunk_payload.buffer); 
     }
     esp_http_client_cleanup(client);
     wifi_disconnect();
