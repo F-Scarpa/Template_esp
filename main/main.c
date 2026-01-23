2b631fcc4d08cfedaafb204a0a766329b05dc870
F-Scarpa <fscarpa20@gmail.com>
2026-01-23 21:38:23 +0100

    added url wildcard

--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "wifi_connect.h"
 #include "nvs_flash.h"
 #include "esp_log.h"
@@ -8,7 +9,13 @@
 #include "cJSON.h"
 
 
-      
+#include "esp_vfs.h"
+#include "esp_vfs_fat.h"
+
+
+static const char *BASE_PATH = "/store";   
+
+
 static const char *TAG = "SERVER";
 
 static esp_err_t on_toggle_led_url(httpd_req_t *req)
@@ -34,34 +41,64 @@ static esp_err_t on_toggle_led_url(httpd_req_t *req)
 
 
 static esp_err_t on_default_url(httpd_req_t *req)     //static = like private member in classes
-{
+{                                                     //http home url page
     ESP_LOGI(TAG,"URL: %s",req->uri);
-    httpd_resp_sendstr(req,"hello world");
+
+    char path[600];
+    sprintf(path, "/store%s", req->uri);      //write a string inside a variable
+
+    FILE *file = fopen(path, "r"); 
+    //if path does not exist                   
+    if(file == NULL)
+    {
+      file = fopen("/store/index.html", "r");    //open index page
+      if(file == NULL)
+      {
+        httpd_resp_send_404(req);   //send error 404
+      }
+      ESP_LOGI(TAG, "STORE/INDEX.HTML NOT FOUND");
+    }
+    char buffer[1024];
+    int bytes_read = 0;
+    while((bytes_read = fread(buffer, sizeof(char), sizeof(buffer),file)) > 0)   //read the file, end file
+                                                                                 //when byres_read is 0
+    {
+      httpd_resp_send_chunk(req, buffer, bytes_read); //send response while buffer is filled with file data
+    }
+    fclose(file);   //close file
+    httpd_resp_send_chunk(req, NULL, 0); //empty http data chunk
+
+    
     return ESP_OK;
 }
 
-static void init_server()
+static void init_server()     // can be used to store paths
 {
   httpd_handle_t server = NULL;   //server handle
   httpd_config_t config = HTTPD_DEFAULT_CONFIG();   //working configuration of our server
 
+  config.uri_match_fn = httpd_uri_match_wildcard;   //enables the use of wildcards in routes, (/*)
+
   ESP_ERROR_CHECK(httpd_start(&server, &config));   //start the server
-// we need to listen to something now
+
+
 
   
-  httpd_uri_t default_url = {   //url = browser address
-    .uri ="/",                  //uri = 
-    .method = HTTP_GET,
-    .handler = on_default_url   //function which will be caleld when someone visits the url
-  };
-  httpd_register_uri_handler(server,&default_url);    //register the http
-
   httpd_uri_t toggle_led_url = {
       .uri = "/api/toggle-led",         //dount use underscores in uri
       .method = HTTP_POST,
       .handler = on_toggle_led_url
     };
   httpd_register_uri_handler(server, &toggle_led_url);
+
+  
+  httpd_uri_t default_url = {   //url = browser address
+    .uri ="/*",                  //accept everything (/*)
+    .method = HTTP_GET,
+    .handler = on_default_url   //function which will be called when someone visits the url
+                                //with /* every url call this func, unless farther in sequence
+  };
+  httpd_register_uri_handler(server,&default_url);    //register the http
 
 }
 
@@ -72,13 +109,26 @@ void start_mdns_service()
   mdns_instance_name_set("LEARN esp32 thing");
 }
 
+void mount_fs()     //setup for fat
+{
+   esp_vfs_fat_mount_config_t esp_vfs_fat_mount_config = {
+        .allocation_unit_size = CONFIG_WL_SECTOR_SIZE,
+        .max_files = 5,
+        .format_if_mount_failed = true,
+    };
+    esp_vfs_fat_spiflash_mount_ro(BASE_PATH, "storage", &esp_vfs_fat_mount_config);   //base_path is /store
+                                                                                      //2. partition name in 
+                                                                                      //partition table
+}
+
 void app_main(void)
 {
   ESP_ERROR_CHECK(nvs_flash_init());
   init_led();
   wifi_connect_init();
-  ESP_ERROR_CHECK(wifi_connect_sta("ssid", "passs", 10000));
+  ESP_ERROR_CHECK(wifi_connect_sta("FRITZ!Box 7530 TM", "BVXZH9GCX4V", 10000));
 
   start_mdns_service();
+  mount_fs();   //
   init_server();
 }
